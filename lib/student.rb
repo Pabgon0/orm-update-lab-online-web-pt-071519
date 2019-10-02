94a10e3dec903e00ed4cd0f6e95056a43500e326
Pablo Gonzalez <pabgon0@gmail.com>
2019-10-02 19:20:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,5 +25,11 @@ class Student
       SQL
     DB[:conn].execute(sql)
   end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
 end
