4460d4250d0c6f17098b5f4cc315330dc7e2c10c
Pablo Gonzalez <pabgon0@gmail.com>
2019-10-02 19:23:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,5 +45,10 @@ class Student
     student.save
     student
   end
+  
+  def update
+    sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
